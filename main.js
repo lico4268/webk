c9749c25a8b80a1c63215f96d4556e504b2217c1
lico4268 <lico4268@gmail.com>
2021-05-05 14:24:10 +0900

    front post add

--- main.js
+++ main.js
@@ -15,34 +15,53 @@ server.use(express.urlencoded({extended : true}));
 
 server.get('/',(req,res) =>{
     //res.send("hello");
-    res.sendFile(__dirname+`/front/main.html`);
+    res.render('main')
 })
 
 server.get('/2',(req,res)=>{
-    res.sendFile(__dirname+`/front/message.html`);
+    //res.sendFile(__dirname+`/front/message.html`);
+    res.render('message')
 })
 
 server.post('/req',(req,res)=>{
     console.log(req.body);
-    //res.send(req.body);
-    //res.send(alert("준비중입니다."));
     const userid = req.body.id;
     const message = req.body.message;
     if(userid&&message){
         conn.query(`INSERT IGNORE INTO test_db.user(userid,contents) VALUES(?,?)`,[userid,message],function(error,results,field){
             if(error){
                 console.log(error);
+                res.json({err:1})
             }
             console.log(results);
+            res.json({status:1})
         })
     }
-    else console.log("NULL") 
+    //else res.send({status:0})
+    else if(!userid) {
+        res.json({status:2})
+        console.log("id NULL")
+    }
+    else if(!message){
+        res.json({status:3})
+        console.log("message NULL")
+    }
     conn.end;
 })
 
-server.get('/3',(req,res)=>{
-    console.log("ejs pages")
-    res.render('1')
+server.get('/view',(req,res)=>{
+    console.log("view pages");
+    res.render('showmsg');
 })
 
+server.post('/view/message',(req,res)=>{
+    
+})
+
+/*
+server.get('/3',(req,res)=>{
+    console.log("ejs pages");
+    res.render('1');
+})
+*/
 server.listen(PORT);
