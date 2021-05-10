471c6a15a47aced84e12d67cd189bfff969a7d44
lico4268 <lico4268@gmail.com>
2021-05-10 20:27:37 +0900

    show messages add

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const server = express();
+const app = express();
 const mysql = require("mysql");
+const router = express.Router();
 const db_config = require('./db.js');
 const conn = db_config.init();
 const ejs = require("ejs")
@@ -8,25 +9,26 @@ const PORT = 5000;
 
 db_config.init();
 
-server.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-server.use(express.json());
-server.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
-server.get('/',(req,res) =>{
+app.get('/',(req,res) =>{
     //res.send("hello");
     res.render('main')
 })
 
-server.get('/2',(req,res)=>{
+app.get('/2',(req,res)=>{
     //res.sendFile(__dirname+`/front/message.html`);
     res.render('message')
 })
 
-server.post('/req',(req,res)=>{
+app.post('/req',(req,res)=>{
     console.log(req.body);
     const userid = req.body.id;
     const message = req.body.message;
+
     if(userid&&message){
         conn.query(`INSERT IGNORE INTO test_db.user(userid,contents) VALUES(?,?)`,[userid,message],function(error,results,field){
             if(error){
@@ -49,19 +51,58 @@ server.post('/req',(req,res)=>{
     conn.end;
 })
 
-server.get('/view',(req,res)=>{
+app.post('/req/check',(req,res)=>{
+    const userid = req.body.id;
+    console.log(req.body.id)
+    var sql ="SELECT * FROM test_db.user WHERE userid = ? "
+    conn.query(sql,[userid],function(err,rows,field){
+        if(err){
+            console.log(err)
+        }
+        //res.send(data)
+        console.log(rows)
+        if(rows.length>0){
+            res.json({dstatus : 1})
+        }
+        else res.json({dstatus : 0})
+    })
+})
+
+app.get('/view',(req,res)=>{
     console.log("view pages");
     res.render('showmsg');
+    res.end();
 })
 
-server.post('/view/message',(req,res)=>{
-    
+app.post('/view',(req,res)=>{
+    const requestId = req.body.userid
+    var sql ="SELECT * FROM test_db.user WHERE userid = ?"
+    conn.query(sql,[requestId],function(err,rows,field){
+        if(err){
+            console.log(err)
+        }
+        else {
+            console.log(rows)
+            res.send(rows)
+
+        }
+    })
 })
 
-/*
-server.get('/3',(req,res)=>{
+
+app.get('/3',(req,res)=>{
     console.log("ejs pages");
-    res.render('1');
+    var sql ="SELECT * FROM test_db.user WHERE userid = ?"
+    var param = ['asdf'];
+    conn.query(sql,param,function(err,rows,field){
+        res.send(rows[2])
+        if(rows.length>0){
+            console.log(rows.length)
+            console.log(rows[1])
+        }
+        else console.log("kasdvni")
+
+    })
 })
-*/
-server.listen(PORT);
+
+app.listen(PORT);
